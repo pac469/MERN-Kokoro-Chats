aaa6216c68571ea83ddafb154a2acd78441ecb8c
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2021-10-17 11:15:30 +0400

    Dealt with dbuser in userHome

--- client/src/Components/UserHome/UserHome.js
+++ client/src/Components/UserHome/UserHome.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './UserHome.css'
 import { useStateValue } from '../../StateProvider'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function UserHome({dbUserId}) {
-    const [{user}, dispatch] = useStateValue();
+function UserHome({ dbUser }) {
+    const [{ user }, dispatch] = useStateValue();
+    console.log('dbUser: ', dbUser)
 
     return (
         <div className="userHome">
-            <img src={user?.photoURL}/>
+            <img src={user?.photoURL} />
             <h1> Hello {user.displayName}!</h1>
-            {dbUserId && <Link className="userHome__button"to={`user/${dbUserId}/room`}> 
+            {dbUser && <Link className="userHome__button" to={`user/${dbUser._id}/room`}>
                 <button>Unlock Your Chats</button>
             </Link>}
-            
+
         </div>
     )
 }
